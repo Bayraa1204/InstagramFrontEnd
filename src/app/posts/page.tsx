a9c2dc0b6ff661cf837c823e8ee7b9af4a27038b
Bayrbaysgalan Chin-erdene <baysaa@Bayrbaysgalans-MacBook-Air.local>
2024-12-06 16:45:33 +0800

    last

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -7,21 +7,26 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
+import Link from "next/link";
 type userType = {
   _id: string;
   username: string;
   email: string;
   profileImg: string;
 };
+type commentType = {
+  _id: string;
+  userId: userType;
+  comment: string;
+}[];
 type postType = {
   _id: string;
   caption: string;
   postImg: string;
   userId: userType;
   like: string;
+  comments: commentType;
 }[];
 const Page = () => {
   const [posts, setPosts] = useState<postType>([]);
@@ -42,10 +47,13 @@ const Page = () => {
   }, []);
   return (
     <div className="bg-black h-max flex-col justify-items-center items-center">
-      <h1 className=" text-[40px] text-white font-sans">Instagram</h1>
+      <h1 className=" text-[40px] text-white font-sans pt-6 p-6">Instagram</h1>
       {posts?.map((post, index) => {
         return (
-          <Card className="bg-black border-gray-800 w-screen rounded-none" key={index}>
+          <Card
+            className="bg-black border-gray-800 w-screen rounded-se-none"
+            key={index}
+          >
             <div className="flex mt-4 items-center gap-3 p-6 pt-0 text-white">
               <Avatar>
                 <AvatarImage src={post.userId.profileImg} />
@@ -53,17 +61,17 @@ const Page = () => {
               </Avatar>
               <div>{post.userId.username}</div>
             </div>
-            <CardContent className="flex justify-center">
+            <CardContent className="flex-col items-center justify-center w=[340px] h-[340px] mb-4">
               <Carousel>
                 <CarouselContent>
-                  <CarouselItem>
-                    <img className="w-screen" src={post.postImg} />
+                  <CarouselItem className="flex justify-center w=[340px] h-[340px]">
+                    <img className="" src={post.postImg} />
                   </CarouselItem>
-                  <CarouselItem>
-                    <img className="w-screen" src={post.postImg} />
+                  <CarouselItem className="flex justify-center w=[340px] h-[340px]">
+                    <img className="" src={post.postImg} />
                   </CarouselItem>
-                  <CarouselItem>
-                    <img className="w-screen" src={post.postImg} />
+                  <CarouselItem className="flex justify-center w=[340px] h-[340px]">
+                    <img className=" " src={post.postImg} />
                   </CarouselItem>
                 </CarouselContent>
               </Carousel>
@@ -78,6 +86,32 @@ const Page = () => {
                 <Bookmark />
               </div>
               <div className="font-bold text-white">548 likes</div>
+              {/* <div className="text-[20px] text-white font-sans">
+                {post.caption}
+              </div> */}
+              <div>
+                {post.comments.slice(0, 2).map((comment) => {
+                  return (
+                    <div
+                      key={comment._id}
+                      className="flex items-center gap-2 mb-3 text-white"
+                    >
+                      <Avatar className="w-[20px] h-[20px]">
+                        <AvatarImage src={comment.userId.profileImg} />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <div className="font-bold">{comment.userId.username}</div>
+                      <div>{comment.comment}</div>
+                    </div>
+                  );
+                })}
+                <Link
+                  href="http://localhost:3000/comments"
+                  className="text-gray-500"
+                >
+                  View all comments
+                </Link>
+              </div>
             </CardFooter>
           </Card>
         );
